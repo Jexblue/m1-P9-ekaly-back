5d50cb91e9dd039a3bbcbd13c799aba589540c37
Jexblue <91774746+Jexblue@users.noreply.github.com>
2022-04-08 12:17:52 +0400

    sakafo Schema update

--- src/models/commande.schema.ts
+++ src/models/commande.schema.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-//import sakafoSchema from "./sakafo.schema";
-import { ISakafo } from "./sakafo.schema";
+/* import this.sakafoSchema from "./sakafo.schema"; */
+import sakafoSchema, { ISakafo } from "./sakafo.schema";
 import { IUser } from "./user.schema";
 
 
-export interface Icommande extends mongoose.Document {
+export interface ICommande extends mongoose.Document {
     
     sakafo: ISakafo;
     client: IUser;
@@ -15,8 +15,15 @@ export interface Icommande extends mongoose.Document {
 
 }
 
-const commandeSchema = new mongoose.Schema {
-    //sakafo: sakafoSchema;
-}
+const commandeSchema = new mongoose.Schema ({
+   
+    sakafo : {type : mongoose.Schema.Types.ObjectId, ref : 'Sakafo'},
+    client : {type : mongoose.Schema.Types.ObjectId, ref : 'User'},
+    livreur : {type : mongoose.Schema.Types.ObjectId, ref: 'User'},
+    dateCommande : Date,
+    fraisLivraison : Number,
+    etat : Number
+    
+});
 
-//export default mongoose.model<ICommande>('Commande', commandeSchema);
+export default mongoose.model<ICommande>('Commande', commandeSchema);
